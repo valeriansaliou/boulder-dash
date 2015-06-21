e91c96724a3dd58a61f531c12c217a59baba1618
Valérian Saliou <valerian@valeriansaliou.name>
2015-06-21 15:43:27 +0200

    Base helper functions
    Signed-off-by: Valérian Saliou <valerian@valeriansaliou.name>

--- src/fr/enssat/BoulderDash/helpers/LevelSaveHelper.java
+++ src/fr/enssat/BoulderDash/helpers/LevelSaveHelper.java
@@ -3,6 +3,20 @@ package fr.enssat.BoulderDash.helpers;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
 import fr.enssat.BoulderDash.models.DisplayableElementModel;
 
 
@@ -52,7 +66,82 @@ public class LevelSaveHelper {
      * Saves the level data into XML storage
      */
     private void saveLevelData() {
-        // TODO
+        DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+
+        try {
+            DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+            Document document = documentBuilder.newDocument();
+
+            Element bdLevel = document.createElementNS("fr.enssat.boulderdash", "bd-level");
+            document.appendChild(bdLevel);
+
+            // append child elements to root element
+            bdLevel.appendChild(dateNode(document, "2015-05-19/15:15:20", "2015-05-19/15:15:20"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Creates the name node
+     *
+     * @param   document  Document
+     * @param   name      'name' child value
+     * @return  Name node
+     */
+    private static Node nameNode(Document document, String name) {
+        return textNode(document, "name", name);
+    }
+
+    /**
+     * Creates the date node
+     *
+     * @param   document       Document
+     * @param   date_created   'date_created' child value
+     * @param   date_modified  'date_modified' child value
+     * @return  Date node
+     */
+    private static Node dateNode(Document document, String date_created, String date_modified) {
+        Element dateElement = document.createElement("date");
+
+        dateElement.setAttribute("format", "utc");
+
+        dateElement.appendChild(textNode(document, "created", date_created));
+        dateElement.appendChild(textNode(document, "modified", date_modified));
+
+        return dateElement;
+    }
+
+    /**
+     * Creates the size node
+     *
+     * @param   document  Document
+     * @param   width     'width' child value
+     * @param   height    'height' child value
+     * @return  Size node
+     */
+    private static Node sizeNode(Document document, String width, String height) {
+        Element sizeElement = document.createElement("size");
+
+        sizeElement.appendChild(textNode(document, "width", width));
+        sizeElement.appendChild(textNode(document, "height", height));
+
+        return sizeElement;
+    }
+
+    /**
+     * Creates a bare text node
+     *
+     * @param   document  Document
+     * @param   name      Element name
+     * @param   value     Element value
+     * @return  Text node
+     */
+    private static Node textNode(Document document, String name, String value) {
+        Element node = document.createElement(name);
+        node.appendChild(document.createTextNode(value));
+
+        return node;
     }
 
     /**
