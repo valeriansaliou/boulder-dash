0adf4bd01ab1db65e69a253af6c2a234065ee8b9
Valérian Saliou <valerian@valeriansaliou.name>
2015-06-25 17:50:57 +0200

    No more Apple UI
    Signed-off-by: Valérian Saliou <valerian@valeriansaliou.name>

--- src/fr/enssat/BoulderDash/Game.java
+++ src/fr/enssat/BoulderDash/Game.java
@@ -1,11 +1,7 @@
 package fr.enssat.BoulderDash;
 
-import com.apple.eawt.Application;
-
 import fr.enssat.BoulderDash.controllers.NavigationBetweenViewController;
 import javax.swing.*;
-
-import java.awt.*;
 
 
 /**
@@ -23,35 +19,10 @@ public class Game {
      * @param  args  Command-line arguments
      */
     public static void main(String[] args) {
-        //setAppleUI(); // Not working
-
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new NavigationBetweenViewController();
             }
         });
-    }
-
-    /**
-     * Sets Apple User Interface elements (look and feel)
-     */
-    public static void setAppleUI() {
-        try {
-            System.setProperty("apple.laf.useScreenMenuBar", "true");
-            System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Boulder Dash");
-
-            Image appIcon = Toolkit.getDefaultToolkit().getImage("res/app/app_icon.png");
-            Application.getApplication().setDockIconImage(appIcon);
-
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException e) {
-            System.out.println("ClassNotFoundException: " + e.getMessage());
-        } catch (InstantiationException e) {
-            System.out.println("InstantiationException: " + e.getMessage());
-        } catch (IllegalAccessException e) {
-            System.out.println("IllegalAccessException: " + e.getMessage());
-        } catch (UnsupportedLookAndFeelException e) {
-            System.out.println("UnsupportedLookAndFeelException: " + e.getMessage());
-        }
     }
 }
